e676ecd3c82e399e702e5f09e4481c57b5643d72
Rohitash Sharma <rohitash.s@cubiclogics.com>
2023-04-05 10:23:33 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,42 @@
+
+export default function Home() {
+
+
+
+
+
+  
+  return (
+    <>
+      <div className="mr-auto align-middle p-5 border-x-sky-950 border-4 border-solid">
+        <div className="font-serif text-center " >
+          <h3 className="qustion">5. Which of the following user-defined header file extension used in c++?</h3>
+          <form className="@apply grid grid-cols-[1fr_1fr] gap-3 text-[15px]">
+            <div className="ans grow ">
+
+            <input type="radio" name="ch" id="input1" />
+            <label htmlFor="input1">hg
+            </label>
+            </div>
+            <div className="ans grow ">
+
+            <input type="radio" name="ch" id="input2" />
+            <label htmlFor="input2">cpp</label>
+            </div>
+            <div className="ans grow ">
+
+            <input type="radio" name="ch" id="input3" />
+            <label htmlFor="input3">h
+            </label>
+            </div>
+            <div className="ans grow ">
+
+            <input type="radio" name="ch" id="input4" />
+            <label htmlFor="input4">hf</label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
