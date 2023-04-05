5232823d97f0a0a5c10b1deed4129a4c30142923
Rohitash Sharma <rohitash.s@cubiclogics.com>
2023-04-05 13:13:12 +0530

    make changes in privious code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8c767161cbdd3b0c"; }
+/******/ 		__webpack_require__.h = function() { return "49ea18b09c5ab912"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import { log } from "console";
 import { useState } from "react";
 
 export default function Home() {
-const [answer, SetAnswer] = useState(" ");
+const [answer, SetAnswer] = useState(false);
+const [index , setIndex]=useState(0)
 const data = [
   {
     qustion:"What is JavaScript?",
-    option:[" JavaScript is a scripting language used to make the website interactive"," JavaScript is an assembly language used to make the website interactive","JavaScript is a compiled language used to make the website interactive","None of Above"],
+    option:["JavaScript is a scripting language used to make the website interactive","JavaScript is an assembly language used to make the website interactive","JavaScript is a compiled language used to make the website interactive","None of Above"],
     ans :"JavaScript is a scripting language used to make the website interactive" 
   },
   {
     qustion:" Which of the following is correct about JavaScript?",
-    option:[" JavaScript is an Object-Based language","JavaScript is Assembly-language"," JavaScript is an Object-Oriented language","JavaScript is a High-level language"],
-    ans :" JavaScript is an Object-Based language" 
+    option:["JavaScript is an Object-Based language","JavaScript is Assembly-language","JavaScript is an Object-Oriented language","JavaScript is a High-level language"],
+    ans :"JavaScript is Assembly-language" 
   },
   {
     qustion:"Arrays in JavaScript are defined by which of the following statements?",
-    option:[" It is an ordered list of values"," It is an ordered list of objects","It is an ordered list of string","It is an ordered list of functions"],
-    ans :"It is an ordered list of values" 
+    option:["It is an ordered list of values","It is an ordered list of objects","It is an ordered list of string","It is an ordered list of functions"],
+    ans :"It is an ordered list of functions" 
   },
   {
     qustion:"Which of the following is not javascript data types?",
@@ -28,19 +28,20 @@ const data = [
     ans :"All of the mentioned" 
   }
 ]
-
-  function handleChange(e:any){
-    console.log(e);    
-  };
+const nextPage = ()=>{
+  if(index<data.length){
+    setIndex(index+1);
+  }
+}
   
   return (
     <>
-      <div className="mr-auto align-middle p-5 border-x-sky-950 border-4 border-solid">
-        <div className="font-serif text-center " >
-          {data.map((item)=>{
+      <div className="mr-auto align-middle p-5 border-x-sky-950 border-4 border-solid ">
+          {/* {data.map((item)=>{
             
-             return(
+            return(
               <>
+               <div className={`font-serif text-center ${answer?"bg-gradient-to-r from-cyan-500 to-blue-500":"bg-gradient-to-r from-white-500 to-white-500"}`} >
                <h3 className="qustion">{item.qustion}</h3>
                <form className="@apply grid grid-cols-[1fr_1fr] gap-3 text-[15px]">
                <div className="ans grow" >
@@ -50,7 +51,7 @@ const data = [
              name="ch"
              id="input1" 
              value={item.option[0]} 
-             onChange={(e)=>{ handleChange(e.target.value===item.ans)}} />
+             onChange={(e)=>{ SetAnswer(e.target.value===item.ans)}} />
             <label htmlFor="input1" >{item.option[0]}
             </label>
             </div>
@@ -61,7 +62,7 @@ const data = [
              name="ch" 
              id="input2" 
              value={item.option[1]}
-             onChange={(e)=>{  handleChange(e.target.value===item.ans)   }} />
+             onChange={(e)=>{ SetAnswer(e.target.value===item.ans)}} />
             <label htmlFor="input2"  >{item.option[1]}</label>
             </div>
             <div className="ans grow ">
@@ -71,7 +72,7 @@ const data = [
             name="ch" 
             id="input3" 
             value={item.option[2]} 
-            onChange={(e)=>{  handleChange(e.target.value===item.ans)   }} />
+            onChange={(e)=>{  SetAnswer(e.target.value===item.ans)}} />
             <label htmlFor="input3" >{item.option[2]}
             </label>
             </div>
@@ -82,15 +83,31 @@ const data = [
             name="ch" 
             id="input4" 
             value={item.option[3]} 
-            onChange={(e)=>{  handleChange(e.target.value===item.ans)   }}  />
+            onChange={(e)=>{SetAnswer(e.target.value===item.ans)}}  />
             <label htmlFor="input4" >{item.option[3]}</label>
             </div>
           </form>
+        </div>
           </>
           )
           }
-          )}
-        </div>
+          )} */}
+          <div className="ques">
+              <h3>{data[index].qustion}</h3>
+          </div>
+          <div>
+            
+            {data[index].option && data[index].option.map((e:any,i:any)=>{
+              return(
+                   <div>
+                 <input type="radio" value={e} id={i} />
+                 <label htmlFor={i}>{e}</label>
+                  </div>
+              )
+          })
+          }
+          </div>
+          <button onClick={nextPage}>Next</button>
       </div>
       </>
   )
